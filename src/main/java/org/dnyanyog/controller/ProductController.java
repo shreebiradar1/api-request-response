cf6b8264af685dca44d147dc7d5ee41c37567b2e
shree biradar <158836387+shreebiradar1@users.noreply.github.com>
2025-02-05 21:10:50 +0530

    Update ProductController.java
    fix: Add product

--- src/main/java/org/dnyanyog/controller/ProductController.java
+++ src/main/java/org/dnyanyog/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.dnyanyog.dto.ProductResponse;
 import org.dnyanyog.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class ProductController {
 	ProductService productService;
 	
 	@PostMapping(path = "/product", produces = {"application/json" , "application/xml"}, consumes = {"application/json" , "application/xml"})
-	public ProductResponse addProduct(ProductRequest product) {
+	public ProductResponse addProduct(@RequestBody ProductRequest product) {
 		return productService.addProduct(product);
 	}
 	
